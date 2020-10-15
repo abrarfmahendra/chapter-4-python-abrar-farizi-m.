d84948da0f391e1b39f87dacc96835e30a6e6653
abrarfmahendra <72193713+abrarfmahendra@users.noreply.github.com>
2020-10-15 22:26:18 +0700

    Add files via upload

---
+++ latihan 2 chapter 4.py
@@ -0,0 +1,13 @@
+import math
+
+jarak = int(input("jarak tempuh = "))
+
+math.pi
+liter = jarak/12
+print("bensin yang di perlukan", liter ,"liter")
+print("jika bensin awal masih kosong")
+banyakisi = liter/50
+print("banyak isi=")
+print(math.floor(banyakisi))
+
+
---
+++ latihan 4 chapter 4.py
@@ -0,0 +1,6 @@
+laki = int(input("banyak siswa laki = "))
+cewe = int(input("banyak siswa cewe = "))
+
+import math
+print(round(3.5))
+print(math.floor(3.5))
